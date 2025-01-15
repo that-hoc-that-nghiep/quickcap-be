d25b6104df17be30f255f12b030a14f621b6780d
thangngo <ngosythang123456@gmail.com>
2025-01-15 13:04:36 +0700

    deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+  getHello() {
+    const superKey = this.configService.get<string>('SUPER_KEY');
+    return {
+      message: superKey,
+    };
   }
 }
