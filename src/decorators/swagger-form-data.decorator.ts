d343d4550a9baf0f851fdd1040b0587583dd8235
thangngo <ngosythang123456@gmail.com>
2025-02-01 22:37:36 +0700

    feat: decorator swagger pagination

--- src/decorators/swagger-form-data.decorator.ts
+++ src/decorators/swagger-form-data.decorator.ts
@@ -1,7 +1,7 @@
 import { UseInterceptors, applyDecorators } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
-import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiQuery } from '@nestjs/swagger';
 import {
   ReferenceObject,
   SchemaObject,
@@ -47,5 +47,38 @@ export function ApiBodyWithSingleFile(
     ApiConsumes('multipart/form-data'),
     ApiBody(api_body),
     UseInterceptors(FileInterceptor(name, local_options)),
+  );
+}
+
+export function ApiDocsPagination(entity: string) {
+  return applyDecorators(
+    ApiQuery({
+      name: 'offset',
+      type: Number,
+      examples: {
+        '0': {
+          value: 0,
+          description: 'Start from 0',
+        },
+        '10': {
+          value: 10,
+          description: `Skip 10 ${entity}s`,
+        },
+      },
+    }),
+    ApiQuery({
+      name: 'limit',
+      type: Number,
+      examples: {
+        '10': {
+          value: 10,
+          description: `Get 10 ${entity}s`,
+        },
+        '50': {
+          value: 50,
+          description: `Get 50 ${entity}s`,
+        },
+      },
+    }),
   );
 }
