1b54af7062a5af5364aa0164f79f41774bccacad
thangngo <ngosythang123456@gmail.com>
2025-02-01 23:07:23 +0700

    feat: secure access for api doc

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { MongoExceptionFilter } from './exception-filters/mongo-exception.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { configSwagger } from './configs/api-docs.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new MongoExceptionFilter());
-  const config = new DocumentBuilder()
-    .setTitle('Quickcap')
-    .setDescription('The Quickcap API description')
-    .setVersion('0.1')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  configSwagger(app);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
--- src/task/task.schema.ts
+++ src/task/task.schema.ts
@@ -1,11 +1,16 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-import { HydratedDocument } from 'mongoose';
+import { Exclude } from 'class-transformer';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type TaskDocument = HydratedDocument<Task>;
 
 @Schema({ versionKey: false })
 export class Task {
+  @Exclude()
+  @ApiProperty()
+  _id: string;
+
   @ApiProperty({ uniqueItems: true })
   @Prop({ required: true, unique: true })
   title: string;
