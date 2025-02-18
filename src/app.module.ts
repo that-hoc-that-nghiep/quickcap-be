0e00980a375e86f2a80537287df237ba863734df
thangngo <ngosythang123456@gmail.com>
2025-02-18 15:08:31 +0700

    feat: validate env by joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,14 +10,37 @@ import { InviteModule } from './invite/invite.module';
 import { MediaModule } from './media/media.module';
 import { ConversationModule } from './conversation/conversation.module';
 import { AuthModule } from './auth/auth.module';
-
+import * as Joi from 'joi';
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        DATABASE_LOCAL_URL: Joi.string().required(),
+        PORT: Joi.number().required(),
+        API_DOCS_URL: Joi.string().required(),
+        API_DOC_USERNAME: Joi.string().required(),
+        API_DOC_PASSWORD: Joi.string().required(),
+        RABBITMQ_URL: Joi.string().required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        REDIS_USERNAME: Joi.string().required(),
+        REDIS_PASSWORD: Joi.string().required(),
+        ACCESS_KEY: Joi.string().required(),
+        SECRET_KEY: Joi.string().required(),
+        BUCKET_REGION: Joi.string().required(),
+        BUCKET_NAME: Joi.string().required(),
+        MAILER_HOST: Joi.string().required(),
+        MAILER_PORT: Joi.number().required(),
+        MAILER_EMAIL: Joi.string().required(),
+        MAILER_PASSWORD: Joi.string().required(),
+      }),
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('DATABASE_URL'),
+        uri: configService.get<string>('DATABASE_LOCAL_URL'),
       }),
       inject: [ConfigService],
     }),
