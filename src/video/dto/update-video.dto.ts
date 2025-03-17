bbb9e124e8ef9f86dc74b3e904e272976254dea3
Hipscarer03 <caoduchiep111@gmail.com>
2025-03-17 14:47:03 +0700

    feat: fix update video

--- src/video/dto/update-video.dto.ts
+++ src/video/dto/update-video.dto.ts
@@ -11,27 +11,22 @@ import {
 
 export class UpdateVideoDto {
   @ApiProperty({ required: false })
-  @IsString()
   @IsOptional()
   title?: string;
 
   @ApiProperty({ required: false })
-  @IsString()
   @IsOptional()
   description?: string;
 
   @ApiProperty({ required: false })
-  @IsString()
   @IsOptional()
   transcript?: string;
 
   @ApiProperty({ required: false })
-  @IsNumber()
   @Min(0, { message: 'Views must be greater than or equal to 0.' })
   @IsOptional()
   views?: number;
   @ApiProperty({ required: false })
-  @IsNumber()
   like?: number;
 
   @ApiProperty({ type: 'boolean', required: false })
@@ -41,8 +36,6 @@ export class UpdateVideoDto {
   nsfwType?: string;
 
   @ApiProperty({ required: false })
-  @IsArray()
-  @ArrayNotEmpty()
   @IsOptional()
   categoryId?: string[];
 }
