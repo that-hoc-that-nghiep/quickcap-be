18b99c918a3978ec8c7222d01b8ce71bce216cc3
ThangNgo123 <ngosythang123456@gmail.com>
2025-03-24 12:01:56 +0700

    fix: ttl redis

--- src/global-module/cache.module.ts
+++ src/global-module/cache.module.ts
@@ -35,7 +35,7 @@ const logger = new Logger('RedisCache');
               return value;
             },
             set: async (key, value, options) => {
-              const ttlInSeconds = 10;
+              const ttlInSeconds = options?.ttl ?? 7;
 
               logger.log(`✅ Cache SET: ${key}, TTL: ${ttlInSeconds} seconds`);
 
@@ -55,7 +55,7 @@ const logger = new Logger('RedisCache');
               return store.reset();
             },
           } as unknown as CacheStore,
-          ttl: 10000,
+          ttl: 7000,
         };
       },
       inject: [ConfigService],
